fe76bbbc97c5fc7faf1ec915eb82663cbfa377a2
OtterSloth <76695983+OtterSloth@users.noreply.github.com>
2023-06-02 05:40:56 -0400

    Create links data

---
+++ script.js
@@ -0,0 +1,145 @@
+const sections = [
+  {
+    title: "Financing",
+    links: [
+      {
+        name: "loans",
+        url: "#",
+      },
+      {
+        name: "Overdraft facilities",
+        url: "#",
+      },
+      {
+        name: "Bank guaranties",
+        url: "#",
+      },
+      {
+        name: "Leasing",
+        url: "#",
+      },
+      {
+        name: "Factoring",
+        url: "#",
+      },
+      {
+        name: "Trade Solutions",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Savings and Investment",
+    links: [
+      {
+        name: "Savings accounts",
+        url: "#",
+      },
+      {
+        name: "Asset Management",
+        url: "#",
+      },
+      {
+        name: "Commercial paper",
+        url: "#",
+      },
+      {
+        name: "Equities",
+        url: "#",
+      },
+      {
+        name: "Invest Products",
+        url: "#",
+      },
+      {
+        name: "Cash Management",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Transaction Banking",
+    links: [
+      {
+        name: "Coporate app",
+        url: "#",
+      },
+      {
+        name: "Internet banking",
+        url: "#",
+      },
+      {
+        name: "Mbile banking",
+        url: "#",
+      },
+      {
+        name: "Alert services",
+        url: "#",
+      },
+      {
+        name: "Accounts",
+        url: "#",
+      },
+      {
+        name: "Cards",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Markets",
+    links: [
+      {
+        name: "Market Reports",
+        url: "#",
+      },
+      {
+        name: "Equity rates",
+        url: "#",
+      },
+      {
+        name: "Commodities",
+        url: "#",
+      },
+      {
+        name: "Equities",
+        url: "#",
+      },
+      {
+        name: "Foreign Exchange",
+        url: "#",
+      },
+      {
+        name: "Investment products",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Calculators and Tools",
+    links: [
+      {
+        name: "Currency converter",
+        url: "#",
+        icon: "",
+      },
+      {
+        name: "Hourly Rate Converter",
+        url: "#",
+        icon: "",
+      },
+      {
+        name: "International trade",
+        url: "#",
+        icon: "",
+      },
+      {
+        name: "IBAN calculator",
+        url: "#",
+        icon: "",
+      },
+    ],
+  },
+]
+
+const linksSection = document.getElementById
